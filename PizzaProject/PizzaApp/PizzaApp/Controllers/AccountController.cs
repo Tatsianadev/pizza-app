d2ff63a6dd3547dea32cfa80d794d860284a2f5d
tatsianadev <tatsianadev@outlook.com>
2022-06-27 17:09:15 +0200

    Login (part1)
    LodinModel added.
    Account -> Login (get and post request) done. Page Login done

--- PizzaProject/PizzaApp/PizzaApp/Controllers/AccountController.cs
+++ PizzaProject/PizzaApp/PizzaApp/Controllers/AccountController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using PizzaApp.Models.Identity;
 
 namespace PizzaApp.Controllers
@@ -53,5 +55,60 @@ namespace PizzaApp.Controllers
 
             return View(model);
         }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await UserManager.FindAsync(model.Email, model.Password);
+                if (user==null)
+                {
+                    ModelState.AddModelError("", "Invalid password");
+                }
+                else
+                {
+                    ClaimsIdentity claims =
+                        await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claims);
+                    if (String.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    return Redirect(returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Login");
+        }
+
     }
 }
